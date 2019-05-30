136737f64b5da94662ec8027e9979b478825e96a
joseaparicio <aldairxd11@hotmail.com>
2019-05-30 16:45:22 -0500

    mejorando login

--- login.php
+++ login.php
@@ -5,27 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <link rel="stylesheet" href="personalizado.css">
+    <link rel="stylesheet" href="configuracion.css">
     
 </head>
 <body>
     <h1>Iniciar Sesion</h1>
+    
 
     <form action="procesar_login.php" method="POST">
-        <div>
-            <label name="user">Usuario:</label>
+        <div class="mod">
+            <label name="usuario">Usuario:</label>
             <input type="text" name="user">
         </div>
 
-        <div>
+        <div class="mod" >
             <label name="contraseña">Contraseña:</label>
             <input type="password" name="contraseña" >
         </div>
 
 
-        <button>Logear</button>
-
+        <button class="boton">Logear</button>
+    
     </form>
+
+
+
 
     
     
