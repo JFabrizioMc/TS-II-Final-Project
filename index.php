187c0b9fb8613284d2c080e50c6b60a3ecfd3b6c
Luis Delzo <alumno@ciis.usil.edu.pe>
2019-05-30 16:21:17 -0500

    Diseño Index v 0.02

--- index.php
+++ index.php
@@ -8,21 +8,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Animal Hope for Life AHL</h1>
+    <h1>Animal Hope for Life</h1>
         <nav class="opciones">
             <a href="nosotros.php">Nosotros</a>
             <a href="login.php">Login</a>
             <a href="registrar_usuario.php">Registro de Usuario</a>
         </nav>
 
-    <img src="imagenes/imgPrincipal.jpg" alt="perro">
+    <img src="imagenes/imgPrincipal.jpg" alt="perro" class="imgPrincipal">
     
     <footer>
         <nav>
-            <a href="acercaDe.php">Acerca de Nosotros</a>
-            <a href="login.php">Login</a>
+            <a href="nosotros.php">Nosotros</a>
             <a href="registrar_usuario.php">Registro de Usuario</a>
         </nav>
+
+        <p>Contactos: 01-3488923.</p>
+        <p>Direccion: Av. la Fontana 550, La Molina 00012 </p>
+        <p>Powered by Taller de Software II bloque tarde - Profesor Jhonatan Chavez</p>
+        <p>Creadores: Luis Delzo Espinoza, Fabrizio Manco, Jose Aparicio(Alias KillerPool)</p>
     </footer>
 </body>
 </html>
