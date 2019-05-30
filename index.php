a1fc473d1c9e999b33f76cc110dc888ced20e59f
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-30 15:36:08 -0500

    creacion de index y style v0.01

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Main Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Animal Hope for Life AHL</h1>
+        <nav>
+            <a href="nosotros.php">Nosotros</a>
+            <a href="login.php">Login</a>
+            <a href="registrar_usuario.php">Registro de Usuario</a>
+        </nav>
+    
+    <footer>
+        <nav>
+            <a href="acercaDe.php">Acerca de Nosotros</a>
+            <a href="login.php">Login</a>
+            <a href="registrar_usuario.php">Registro de Usuario</a>
+        </nav>
+    </footer>
+</body>
+</html>
