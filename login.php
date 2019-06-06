3d5cdc4acd66eeb0b18b439324db7e9118f09f34
joseaparicio <aldairxd11@hotmail.com>
2019-06-06 01:19:41 -0500

    mejorando login

--- login.php
+++ login.php
@@ -12,7 +12,17 @@
 <body>
     <h1>Iniciar Sesion</h1>
     <form action="procesar_login.php" method="POST">
+            
         <div class="padre_login">
+            <?php if(isset($_GET['falta'])){ ?>
+                <p style="color: red">Debe ingresar el correo o la contraseña </p>
+            <?php } ?>            
+            <?php if(isset($_GET['error'])){ ?>
+                <p style="color: red">La contraseña o el correo no coinciden , intentelo de nuevo</p>
+            <?php } ?>
+            <?php if(isset($_GET['noexiste'])){ ?>
+                <p style="color: red">El correo no existe en nuestro base de datos</p>
+            <?php } ?>
             <div class="div1">
                 <label name="usuario">Correo Electronico</label>
                 <div class="asd">
@@ -34,9 +44,7 @@
                     <div>
                         <input type="password" name="contraseña" placeholder="Contraseña...">
                     </div>
-                    
                 </div>
-
             </div>
 
 
--- procesar_login.php
+++ procesar_login.php
@@ -1,16 +1,40 @@
 <?php
-$user = $_POST["user"];
+$pdo = new PDO("mysql:host=localhost;dbname=proyecto;charset=utf8","root","");
+
+$user = $_POST["usuario"];
 $password = $_POST["contraseña"];
 
+$sql="SELECT * FROM usuarios WHERE email='$user' ";
 
-if ($user== "aldairxd11@hotmail.com" && $password == "123" ) { 
-    session_start();
-    $_SESSION["usuario"] = $user;
-    header("Location: index.php");
+$resultado = $pdo->prepare($sql);
+$resultado->execute();
+$numero_rows = $resultado->fetchAll();
+
+
+
+if(count($numero_rows) > 0 ){
+    foreach($numero_rows as $fila){
+        if(password_verify($password, $fila['contraseña']) ) {
+            header("Location: index.php"); 
+        }
+        else{
+            header("Location: login.php?error=contraseñaincorrecta");
+        }
+    }
 }
 
-else{ # Usuario incorrecto
-    header("Location: index.php?m=1");
+else if ($user=="" || $password==""){
+    header("Location: login.php?falta=datosfaltantes");
 }
+
+else {
+    header("Location: login.php?noexiste=corre_no_existe");
+}
+
+
+
+
+
+
  
 ?>
