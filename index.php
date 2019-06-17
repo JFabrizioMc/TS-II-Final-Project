0921df0993b0af09c4226907ab66d76acc1739e2
Luis Delzo E <ldelzo.e@gmail.com>
2019-06-16 22:32:10 -0500

    cambio de pagina Index/ nuevo modelo

--- index.php
+++ index.php
@@ -4,47 +4,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link rel="stylesheet" href="estilos/style.css">
-    <title class="title">Animal Hope for Life</title>
-    
+    <link rel="stylesheet" href="estilos/index_style.css">
+    <link href='https://fonts.googleapis.com/css?family=Allan' rel='stylesheet'>
+    <title>Pagina Principal</title>
 </head>
 <body>
-    <div class="flex-container1">
-        
-        <img class="logo" src="imagenes/logo.png">
+    <div id="barraSuperior">
+        <img id="logo" src="imagenes/logo.png">
         <nav>
             <a href="nosotros.php">Que es AHL?</a>
             <a href="login.php">Login</a>
             <a href="registrar_usuario.php">Registrar</a>
             <a href="donar.php">Donaciones</a>     
         </nav>
-
-        <div class="mensaje">
-            <p>Estoy a favor del derecho de los animales, al igual que del derecho de los humanos. Ese es el camino de un ser humano completo (Abraham Lincoln)</p>
-        </div>
-        
-        <img src="imagenes/slide1.jpg">
     </div>
 
-    
-    <div class="flex-container2">
-
-        <div class="mensaje2">
-            <p>Deseas ayudar a un pequeño amigo? Adopta mediante AHL y llena de amor tu vida!</p>
+    <div class="contenedorDeslizable">        
+        <!--Imagenes a usar en la ventana deslizable-->
+        <div class="slide fade">
+            <p id="citaSlide1">Estoy a favor del derecho de los animales, al igual que del derecho de los humanos. Ese es el camino de un ser humano completo (Abraham Lincoln)</p>
+            <img src="imagenes/slide1.jpg" style="width:100%">
         </div>
 
-        <div class="mensaje3">
-            <p>Haz que no sufran, aportando con un pequeño donativo.</p>
+        <div class="slide fade">
+            <p id="citaSlide2"> Nuestros compañeros perfectos nunca tienen menos de cuatro patas (Colette)</p>
+            <img src="imagenes/slide2.jpg" style="width:100%">
         </div>
-        <a href="login.php"><img src="imagenes/perroyorki.jpg" id="img2"></a>
-        <a href="donar.php"><img src="imagenes/gatoLisiado.jpg" id="img3"></a>
+        
+        <div class="slide fade">
+            <p id="citaSlide3">Los ojos de un animal tienen el poder de hablar un gran lenguaje(Martin Buber)</p>
+            <img src="imagenes/slide3.jpg" style="width:100%">
+        </div>
+
+        <div class="slide fade">
+            <p id="citaSlide4">Los animales son muy buenos amigos; no hacen preguntas, no critican (George Eliot)</p>
+            <img src="imagenes/slide4.jpg" style="width:100%">
+        </div>
+       
+        <div class="slide fade">
+            <p id="citaSlide5">Hasta que uno no ha amado un animal, una parte del alma sigue sin despertar (Anatole France)</p>
+            <img src="imagenes/slide5.jpg" style="width:100%">
+        </div>
+        <!--Botones Next y Previous-
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>-->
+
+        <!--Implementacion de JavaScript para el slide-->
+        <script>
+            var slideIndex = 0;
+            showSlides();
+
+            function showSlides() {
+                var i;
+                var slides = document.getElementsByClassName("slide");
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none"; 
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {slideIndex = 1} 
+                slides[slideIndex-1].style.display = "flex"; 
+                setTimeout(showSlides, 5000); // Mulplicar 1000 * cantidad de segundos en que cambia la imagen
+            }
+        </script>
     </div>
 
     <footer>
         <p>Desarrolladores: Luis Delzo, Fabrizio Manco, Aparicio Alan.</p>
         <p>2019 - Peru</p>
     </footer>
-
+    
 </body>
 </html>
---
+++ oldIndex.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos/style.css">
+    <title class="title">Animal Hope for Life</title>
+    
+</head>
+<body>
+    <div class="flex-container1">
+        
+        <img class="logo" src="imagenes/logo.png">
+        <nav>
+            <a href="nosotros.php">Que es AHL?</a>
+            <a href="login.php">Login</a>
+            <a href="registrar_usuario.php">Registrar</a>
+            <a href="donar.php">Donaciones</a>     
+        </nav>
+
+        <div class="mensaje">
+            <p>Estoy a favor del derecho de los animales, al igual que del derecho de los humanos. Ese es el camino de un ser humano completo (Abraham Lincoln)</p>
+        </div>
+        
+        <img src="imagenes/slide1.jpg">
+    </div>
+
+    
+    <div class="flex-container2">
+
+        <div class="mensaje2">
+            <p>Deseas ayudar a un pequeño amigo? Adopta mediante AHL y llena de amor tu vida!</p>
+        </div>
+
+        <div class="mensaje3">
+            <p>Haz que no sufran, aportando con un pequeño donativo.</p>
+        </div>
+        <a href="login.php"><img src="imagenes/perroyorki.jpg" id="img2"></a>
+        <a href="donar.php"><img src="imagenes/gatoLisiado.jpg" id="img3"></a>
+    </div>
+
+    <footer>
+        <p>Desarrolladores: Luis Delzo, Fabrizio Manco, Aparicio Alan.</p>
+        <p>2019 - Peru</p>
+    </footer>
+
+</body>
+</html>
